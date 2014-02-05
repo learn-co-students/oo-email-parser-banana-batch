821e50317fb00861ae5b147e488e604d7a908f5e
Spencer Rogers <spencer1248@gmail.com>
2014-02-05 13:45:55 -0500

    Initial commit.

---
+++ email_parser.rb
@@ -0,0 +1,3 @@
+# Build a class EmailParser that accepts a string of unformatted 
+# emails. The parse method on the class should separate them into
+# unique email addresses. The delimiters to support are commas (',')
---
+++ spec/email_parser_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "EmailParser" do
+  describe '#parser' do
+    
+    it "parses CSV emails" do
+      emails = "avi@test.com, arel@test.com"
+      expect(EmailParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com"])
+    end
+
+    it "parses space delimited emails" do
+      emails = "avi@test.com arel@test.com"
+
+      expect(EmailParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com"])
+    end
+
+    it "parses both CSV and spac delimited emails" do
+      emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
+
+      expect(EmailParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"]) 
+    end
+
+    it 'parses and removes duplicate emails' do
+      emails = "avi@test.com, avi@test.com"
+      
+      expect(EmailParser.new(emails).parse).to eq(["avi@test.com"])
+    end
+
+  end
+end
