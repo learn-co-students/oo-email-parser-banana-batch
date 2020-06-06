22e45544692aaf01ee107a035171f26adc853f48
Liz Burton <liz.burton147@gmail.com>
2020-06-06 08:36:58 -0400

    Fix name of class in notes

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-# Build a class EmailParser that accepts a string of unformatted 
+# Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
